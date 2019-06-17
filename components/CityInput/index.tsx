f9c0dd6fed217cccd2c923d93a659532d2dd406e
teerryn <slaughterer90@gmail.com>
2019-06-17 14:23:09 +0100

    Implement city input

---
+++ components/CityInput/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Downshift from 'downshift';
+import { City } from '../generated/apolloComponents';
+
+interface IProps {
+  cities: City[];
+  handleChange: (city: City) => null;
+}
+
+const getSuggestions = (value: string | null, list: City[]) => {
+  if (!value) {
+    return [];
+  }
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  const suggestedList = list
+    .filter(
+      city => city.name.toLowerCase().slice(0, inputLength) === inputValue
+    )
+    .sort((cityA, cityB) => cityA.name.length - cityB.name.length);
+
+  return inputLength < 2 ? [] : suggestedList;
+};
+
+const CityInput: React.FC<IProps> = props => (
+  <Downshift
+    onChange={props.handleChange}
+    itemToString={(item: City | null) => (item ? item.name : '')}
+  >
+    {({
+      getInputProps,
+      getLabelProps,
+      getMenuProps,
+      getItemProps,
+      isOpen,
+      inputValue,
+      highlightedIndex,
+      selectedItem,
+    }) => (
+      <div>
+        <label {...getLabelProps()}>{props.children}</label>
+        <input {...getInputProps()} />
+        <ul {...getMenuProps()}>
+          {isOpen &&
+            getSuggestions(inputValue, props.cities).map((item, index) => (
+              <li
+                key={item.id}
+                {...getItemProps({
+                  index,
+                  item,
+                  style: {
+                    backgroundColor:
+                      highlightedIndex === index ? 'lightgray' : undefined,
+                    fontWeight: selectedItem === item ? 'bold' : 'normal',
+                  },
+                })}
+              >
+                {`${item.name}, Distrito de ${item.district}`}
+              </li>
+            ))}
+        </ul>
+      </div>
+    )}
+  </Downshift>
+);
+
+export default CityInput;
