a0ead9df5c829fab83a1a830aa33c1d876b0451f
teerryn <slaughterer90@gmail.com>
2019-06-19 20:50:54 +0100

    Update types

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,4 @@
+import { Hit } from 'react-instantsearch-core';
 export type JobMeQuery = { __typename?: 'Job' } & Pick<
   JobType,
   | 'id'
@@ -16,3 +17,24 @@ export type JobMeQuery = { __typename?: 'Job' } & Pick<
     >;
     tags: Array<Maybe<{ __typename?: 'Tag' } & Pick<Tag, 'id' | 'name'>>>;
   };
+
+export interface IDoc extends Hit {
+  city: string;
+  shortDescription: string;
+  company: string;
+  slug: string;
+  title: string;
+  updated_at: number;
+  url: string | null;
+  categories: string[];
+  techs: string[];
+  _tags: string[];
+  _geoloc: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export function instanceOfJob(object: any): object is JobMeQuery {
+  return 'status' in object;
+}
