eeb57415a8703ff7ea77e57342dc79619d79edf6
teerryn <slaughterer90@gmail.com>
2019-06-26 14:37:45 +0100

    fix saveHistory function that was saving the wrong items

--- lib/history.ts
+++ lib/history.ts
@@ -4,15 +4,23 @@ export async function saveHistory(searchTerm: string, city: string) {
   const searchUrl = `${MY_DOMAIN}/jobs?job=${searchTerm}&city=${city}`;
   const historyString = await localStorage.getItem('history');
   if (historyString) {
-    const historyList = JSON.parse(historyString);
-    const searchExists = historyList && historyList.indexOf(searchUrl);
-    historyList.slice(0, +searchExists);
-    historyList.slice(+searchExists + 1);
-    const newHistoryList = [historyList.slice(-4), searchUrl];
-    return await localStorage.setItem(
-      'history',
-      JSON.stringify(newHistoryList)
-    );
+    const historyList: string[] = JSON.parse(historyString);
+    const matchedIndex = historyList.indexOf(searchUrl);
+    if (matchedIndex >= 0) {
+      const head = historyList.slice(0, matchedIndex);
+      const tail = historyList.slice(matchedIndex + 1);
+      const listWithoutElement = [...head, ...tail];
+      const newHistoryList = [...listWithoutElement.slice(-4), searchUrl];
+      return await localStorage.setItem(
+        'history',
+        JSON.stringify(newHistoryList)
+      );
+    } else {
+      return await localStorage.setItem(
+        'history',
+        JSON.stringify([...historyList.slice(-4), searchUrl])
+      );
+    }
   }
 
   return await localStorage.setItem('history', JSON.stringify([searchUrl]));
