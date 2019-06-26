6f35e485898760d7e1b9578a0ca041e8ad77fe2f
teerryn <slaughterer90@gmail.com>
2019-06-26 22:40:23 +0100

    Display saved history

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NextFunctionComponent } from 'next';
 import Link from 'next/link';
 import CityInput from '../components/CityInput';
@@ -7,7 +7,7 @@ import { CITIES_QUERY } from '../graphql/cities/query';
 import { CitiesQuery, City } from '../components/generated/apolloComponents';
 import Input from '../components/styles/components/Input';
 import checkLoggedIn from '../lib/checkLoggedIn';
-import { saveHistory } from '../lib/history';
+import { saveHistory, getHistory, IHistory } from '../lib/history';
 import Router from 'next/router';
 
 interface IProps {
@@ -19,6 +19,8 @@ const Index: NextFunctionComponent<IProps, IProps, AppContext> = ({
 }) => {
   const [city, setCity] = useState<City>();
   const [job, setJob] = useState<string>();
+  const [history, setHistory] = useState<IHistory[]>();
+
   const handleClick = async () => {
     if (job && city) {
       await saveHistory(job, city.name);
@@ -29,15 +31,54 @@ const Index: NextFunctionComponent<IProps, IProps, AppContext> = ({
     });
   };
 
-  return (
-    <div style={{ display: 'flex' }}>
-      <Input onChange={e => setJob(e.target.value)} />
-      <CityInput
-        cities={cities}
-        handleChange={selectedCity => setCity(selectedCity)}
-      />
+  useEffect(() => {
+    async function get() {
+      const savedHistory = await getHistory();
+      setHistory(savedHistory);
+    }
+    get();
+  }, []);
 
-      <a onClick={handleClick}>Procurar</a>
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ display: 'flex' }}>
+        <Input onChange={e => setJob(e.target.value)} />
+        <CityInput
+          cities={cities}
+          handleChange={selectedCity => setCity(selectedCity)}
+        />
+
+        <a onClick={handleClick}>Procurar</a>
+      </div>
+      <div>
+        <h3>Pesquisas Anteriores</h3>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+          }}
+        >
+          {history &&
+            history.map(
+              (
+                { job: jobItem, city: cityItem }: { job: string; city: string },
+                index
+              ) => {
+                return (
+                  <Link
+                    key={index}
+                    href={`jobs?job=${jobItem}&city=${cityItem}`}
+                  >
+                    <a>
+                      {jobItem} - {cityItem}
+                    </a>
+                  </Link>
+                );
+              }
+            )}
+        </div>
+      </div>
     </div>
   );
 };
